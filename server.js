e46d80aa38a3a5a3ecd1f8a3a7dda49a2d8b3d9d
UniverseStark <jty90k@gmail.com>
2021-03-08 16:13:08 +0900

    상품 전체검색 및 조건검색 및 정렬

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const models = require("./models");
 const port = 8080;
 
 // app.use는 어떤 app에 설정을 해준다. 그 떄 express.json() json으로 정보를 전달했 듯이 express에서도 json 형식으로 데이터 전송한다.
@@ -9,48 +10,82 @@ app.use(express.json());
 app.use(cors());
 // 람다 함수 / 아래와 같은 경로로 함수가 .get 요청이 왔을 때  두 번째 익명 함수가 실행이 된다.
 app.get("/products", (req, res) => {
-  const query = req.query;
-  console.log(query);
-  res.send({
-    products: [
-      {
-        id: 1,
-        name: "농구공",
-        price: 100000,
-        seller: "조던",
-        imageUrl: "images/products/basketball1.jpeg",
-      },
-      {
-        id: 2,
-        name: "축구공",
-        price: 50000,
-        seller: "메시",
-        imageUrl: "images/products/soccerball1.jpg",
-      },
-      {
-        id: 3,
-        name: "키보드",
-        price: 10000,
-        seller: "그랩",
-        imageUrl: "images/products/keyboard1.jpg",
-      },
-    ],
-  });
+  // 제품에 있는 모든 파일을 검색(findALL)할 수 없기 때문에 page nation 한정을 걸어둔다. (lmit: 10, where : {}, order: 정렬) 조건을 걸어 where 문도 가능하다.
+  models.Product.findAll({
+    //limit : 10,
+    //where : {}
+    order: [["createdAt", "DESC"]],
+    // 상품 정보를 업데이트할 때는 description을 받지 않는다. 대신 ㅁ (62줄 아래로 이동)
+    attributes: ["id", "name", "price", "createdAt", "seller"],
+  })
+    .then((result) => {
+      console.log("PRODUCT : ", result);
+      res.send({
+        products: result,
+      });
+    })
+    .catch((error) => {
+      console.error(err);
+      res.send("에러 발생");
+    });
 });
 
 app.post("/products", (req, res) => {
   const body = req.body;
-  res.send({
-    body,
-  });
+  const { name, description, price, seller } = body;
+  if (!name || !description || !price || !seller) {
+    res.send("모든 작성폼을 작성해주세요.");
+  }
+  models.Product.create({
+    name,
+    description,
+    price,
+    seller,
+  })
+    .then((result) => {
+      console.log("상품 생성 결과 :", result);
+      res.send({
+        result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send("상품 업로드에 문제가 발생했습니다.");
+    });
 });
 
-app.get("/products/:id/events/:eventId", (req, res) => {
+app.get("/products/:id", (req, res) => {
   const params = req.params;
-  const { id, eventId } = params;
-  res.send(`id는 ${id}와 ${eventId}입니다.`);
+  const { id } = params;
+  //findALl: 전체 찾기 // findOne 하나만 찾기
+  // ㅁ 단 여기서는 상세 상품 정보를 얻을 수 있다.
+  models.Product.findOne({
+    where: {
+      id: id,
+    },
+  })
+    .then((result) => {
+      console.log("PRODUCT: ", result);
+      res.send({
+        product: result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send("상품 조회에 에러가 발생했습니다.");
+    });
 });
 
 app.listen(port, () => {
   console.log("그랩의 쇼핑몰 서버가 돌아가고 있습니다.");
+  models.sequelize
+    .sync()
+    .then(() => {
+      console.log("DB 연결 성공!");
+    })
+    .catch((error) => {
+      console.error(err);
+      console.log("DB연결 에러!");
+      process.exit();
+    });
 });
